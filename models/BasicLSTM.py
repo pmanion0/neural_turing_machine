d44cdadc44baf2222287f12385f605656cd193e1
Patrick Manion <patrickjmanion@gmail.com>
2019-05-24 04:03:28 +0000

    Update LSTM for sequence prediction

--- models/BasicLSTM.py
+++ models/BasicLSTM.py
@@ -9,25 +9,38 @@ class BasicLSTM(nn.Module):
     ''' Basic single-LSTM with output calculated using a single
         linear layer with a softmax activation '''
     
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, output_length = 1):
         ''' Init the LSTM '''
         super(BasicLSTM, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size)
         self.hidden_to_output = nn.Linear(hidden_size, output_size)
         self.hidden_size = hidden_size
+        self.output_length = output_length
+        self.output_size = output_size
         self.softmax = nn.LogSoftmax(dim=2)
+        
         
     def forward(self, input, hidden):
         ''' Run a forward path for a single number of sequence of numbers
         
+        Hidden is made up of (hidden, cell_state)
+        
         Returns:
             output and hidden layer after last sequence input
         '''
-        # Hidden is made up of (hidden, cell_state)
+        output = []
+        
         history, hidden = self.lstm.forward(input, hidden)
-        output = self.hidden_to_output(hidden[0])
-        output = self.softmax(output)
+        
+        output.append(self.softmax(self.hidden_to_output(hidden[0])))
+        
+        for j in range(1, self.output_length):
+            history, hidden = self.lstm.forward(output[-1], hidden)
+            output.append(self.softmax(self.hidden_to_output(hidden[0])))
+            
+        output = torch.cat(output, dim=0)
         return output, hidden
+    
     
     def init_hidden(self):
         ''' Returns new hidden layers for the start of a new sequence '''
