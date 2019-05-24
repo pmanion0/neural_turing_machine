f041919a26153da70d30bbe69a47a8b2c44acf63
Patrick Manion <patrickjmanion@gmail.com>
2019-05-24 02:56:41 +0000

    Working RNN for sequence prediction

--- models/BasicRNN.py
+++ models/BasicRNN.py
@@ -8,11 +8,13 @@ from torch.nn import functional as f
 class BasicRNN(nn.Module):
     ''' Basic RNN with a softmax output layer '''
     
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, hidden_size, output_size, output_length = 1):
         ''' Init the RNN '''
         super(BasicRNN, self).__init__()
         
         self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.output_length = output_length
         
         self.input_to_output = nn.Linear(input_size + hidden_size, output_size)
         self.input_to_hidden = nn.Linear(input_size + hidden_size, hidden_size)
@@ -24,12 +26,18 @@ class BasicRNN(nn.Module):
         Returns:
             output and hidden layer after last sequence input
         '''
+        output = torch.zeros(self.output_length, 1, self.output_size)
+        
         for i in range(input.shape[0]):
             combined_input = torch.cat((input[i].unsqueeze(0), hidden), dim=2)
             hidden = self.input_to_hidden(combined_input)
             
-        output = self.input_to_output(combined_input)
-        output = self.softmax(output)
+        output[0:1] = self.softmax(self.input_to_output(combined_input))
+        
+        for j in range(1,self.output_length):
+            combined_input = torch.cat((output[j].unsqueeze(0), hidden), dim=2)
+            hidden = self.input_to_hidden(combined_input)
+            output[j:j+1] = self.softmax(self.input_to_output(combined_input))
         
         return output, hidden
     
