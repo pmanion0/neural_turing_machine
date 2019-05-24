3e4b549fdd978962fe5adefe39c875891a212635
Patrick Manion <patrickjmanion@gmail.com>
2019-05-24 04:36:40 +0000

    Clean-up run.py documentation, structure

--- run.py
+++ run.py
@@ -1,3 +1,24 @@
+''' Script for running RNN experiments on command line
+
+Example:
+
+    $ python3 run.py <model_type> <task_type> > test_out.txt
+
+Args:
+    model_type: string indicating the RNN model to use, options:
+        BasicRNN, BasicLSTM, and NTM_LSTM
+    
+    task_type: string indicating the toy task to use, options:
+        Single, StartSequence3, StartSequence20
+        
+Returns:
+    prints periodic outputs of the average loss rate over the
+    last batch of examples run through the model, e.g.
+    
+    $ [0] Error: tensor(0.0022, device='cuda:0', grad_fn=<DivBackward0>)
+    $ [1000] Error: tensor(1.9112, device='cuda:0', grad_fn=<DivBackward0>)
+'''
+
 import torch
 import sys
 
@@ -8,54 +29,35 @@ from models.BasicRNN import BasicRNN
 from models.BasicLSTM import BasicLSTM
 from models.NTM_LSTM import NTM_LSTM
 from tasks.Numbers import Numbers
+from training import train_model
 
+# Read Command Line Arguments: 
 model = sys.argv[1]
 task = sys.argv[2]
 
-def train_model(model, number_tool, criterion, optim, train_size = 100e3, stream_size = 200, print_interval = 1e3):
-    ''' Runs a full training pass for a given model '''
-    
-    train_size = int(train_size)
-    print_interval = int(print_interval)
-    error_sum = 0
-    model_device = next(model.parameters()).device
-
-    for i in range(train_size):
-        # Generate a new random sequence for training
-        stream = number_tool.create_stream(stream_size)
-        obs = number_tool.encode_stream(stream).to(model_device)
-
-        # Reset the model gradients and hidden layer
-        model.zero_grad()
-        hidden = model.init_hidden()
-        
-        # Score the model
-        output, hidden = model.forward(obs, hidden)
-
-        # Determine the target output and calculate the loss
-        goal = number_tool.get_stream_goal(stream)
-        target = torch.LongTensor([goal]).to(model_device)
-        loss = criterion(output.squeeze(0), target)
-        
-        # Run backprop with the errors and update model/trackers
-        loss.backward()
-        optim.step()
-        error_sum += loss
-
-        if i % print_interval == 0:
-            print('[' + str(i) + '] Error: ' + str(error_sum / print_interval))
-            error_sum = 0
 
 # Number Generator + Goal Setup
 max_number = 9
-goal_func = lambda stream: stream[0]
-goal_dim = max_number+2
+criterion = nn.NLLLoss()
+device = torch.device('cuda') if torch.cuda.is_available else torch.device('cpu')
 
+setup_kwargs = {
+    'train_size': 100e3,
+    'number_tool': None,
+    'criterion': criterion,
+    'stream_size': 200,
+    'model': None,
+    'optim': None,
+    'print_interval': 1e3
+}
+
+
+# Determine the Task to Train On
 if task == 'Single':
     number_tool = Numbers(
         max_number,
         reset_value_func = lambda x: randint(0,max_number),
-        goal_func = goal_func
+        goal_func = lambda stream: stream[0]
     )
 
 elif task == 'StartSequence3':
@@ -70,32 +72,30 @@ elif task == 'StartSequence20':
         increment_func = lambda incr: randint(0,9),
         goal_func = lambda stream: stream[0:20]
     )
+else:
+    print("TASK IS NOT RECOGNIZED! ERROR!")
 
     
-# Training Size
-train_size = 100e3
-criterion = nn.NLLLoss()
+# Add Number Tool to Setup
+setup_kwargs['number_tool'] = number_tool
+input_dim = number_tool.get_dim()
+goal_dim = number_tool.get_dim()
 
-setup_kwargs = {
-    'train_size': train_size,
-    'number_tool': number_tool,
-    'criterion': criterion,
-    'stream_size': 200,
-    'model': None,
-    'optim': None
-}
+fake_stream = number_tool.create_stream(setup_kwargs['stream_size'])
+fake_goal = number_tool.get_stream_goal(fake_stream)
+goal_length = len(fake_goal)
 
-device = torch.device('cuda') if torch.cuda.is_available else torch.device('cpu')
 
+# Determine Model + Optimization to Use
 if model == 'BasicRNN':
-    rnn = BasicRNN(number_tool.get_dim(), 5, goal_dim)
+    rnn = BasicRNN(input_dim, 5, goal_dim, output_length = goal_length)
     rnn_optim = torch.optim.SGD(rnn.parameters(), lr = 0.001)
 	
     setup_kwargs['model'] = rnn
     setup_kwargs['optim'] = rnn_optim
 
 elif model == 'BasicLSTM':
-    lstm = BasicLSTM(number_tool.get_dim(), 5, goal_dim)
+    lstm = BasicLSTM(input_dim, 5, goal_dim, output_length = goal_length)
     lstm.to(device)
 	
     lstm_optim = torch.optim.SGD(lstm.parameters(), lr = 0.001, momentum = 0.9)
@@ -107,7 +107,7 @@ elif model == 'NTM_LSTM':
     memory_banks = 10
     memory_dim = 20
 	
-    ntm = NTM_LSTM(number_tool.get_dim(), 5, goal_dim, memory_banks, memory_dim)
+    ntm = NTM_LSTM(input_dim, 5, goal_dim, memory_banks, memory_dim)
     ntm_optim = torch.optim.SGD(ntm.parameters(), lr = 0.001)
 	
     setup_kwargs['model'] = ntm
@@ -116,4 +116,6 @@ elif model == 'NTM_LSTM':
 else:
     print("MODEL IS NOT RECOGNIZED! ERROR!")
 
-train_model(**setup_kwargs)#, print_interval = 1e2)
+
+# Begin Training the Model!
+train_model(**setup_kwargs)
